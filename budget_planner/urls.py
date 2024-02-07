763fec41177c30c843dbb38c38c0e903f9d1348d
Paweł Perenc <57562561+perqu@users.noreply.github.com>
2024-02-07 11:53:10 +0100

    add docker-compose, update dockerfile

--- budget_planner/urls.py
+++ budget_planner/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from .views import check_database_connection
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "check-connection/", check_database_connection, name="check_database_connection"
+    ),
 ]
